fa582759f10788115bcd80a78af7a6d37ab85bf2
lixiang30 <534551900@qq.com>
2019-02-20 22:02:27 +0800

    日志模块的配置

--- ihome/api_v1_0/demo.py
+++ ihome/api_v1_0/demo.py
@@ -1,6 +1,16 @@
 
 from . import api
+import logging
+from flask import current_app
 
 @api.route("/index")
 def index():
+    # logging.error("xxx") # 错误级别
+    # logging.warn("xxxx") # 警告级别
+    # logging.info("xxx") # 消息提示级别
+    # logging.debug("xxx") # 调试级别
+    current_app.logger.error("error msg")
+    current_app.logger.warn("warn msg")
+    current_app.logger.info("info msg")
+    current_app.logger.debug("debug msg")
     return "index page"
